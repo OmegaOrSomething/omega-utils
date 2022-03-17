6a6e1083abb1fd2f0cdb9334b08a397df59b5474
OmegaOrSomething <95863526+OmegaOrSomething@users.noreply.github.com>
2022-03-17 11:18:35 +0530

    Added new function multiQueryIncludes and renamed multiQuery to multiQueryStartsWith

--- index.js
+++ index.js
@@ -47,9 +47,9 @@ const arrayRandomize = (arr) => {
  *
  * @param {Message} msg A variable which is <Discord.Message>
  * @param {String} type The type of value [VALID TYPES: MEMBERS | ROLES]
- * @param {String[]} query The query to filter
+ * @param {String} query The query to filter
  */
-const multiQuery = (msg, type, query) => {
+const multiQueryStartsWith = (msg, type, query) => {
   if (!msg) throw new Error(`A <Discord.Message> is required`);
   if (!type) throw new Error(`A Query Type is required`);
   if (!query) throw new Error(`A Query is required`);
@@ -61,7 +61,7 @@ const multiQuery = (msg, type, query) => {
   switch (type) {
     case "members":
       const temp = msg.guild.members.cache.filter((m) =>
-        m.user.username.toLowerCase().startsWith(query)
+        m.user.username.toLowerCase().startsWith(query.toLowerCase())
       );
       const allMem = temp.toJSON();
       allMem.forEach((mem) => {
@@ -71,7 +71,50 @@ const multiQuery = (msg, type, query) => {
 
     case "roles":
       const temporary = msg.guild.roles.cache.filter((r) =>
-        r.name.toLowerCase().startsWith(query)
+        r.name.toLowerCase().startsWith(query.toLowerCase())
+      );
+      const allRoles = temporary.toJSON();
+      allRoles.forEach((role) => {
+        results.push(role);
+      });
+      break;
+
+    default:
+      throw new Error(`The valid Query Types are [MEMBERS] and [ROLES]`);
+  }
+  return results;
+};
+
+/**
+ *
+ * @param {Message} msg
+ * @param {String} type
+ * @param {String} query
+ * @returns
+ */
+const multiQueryIncludes = (msg, type, query) => {
+  if (!msg) throw new Error(`A <Discord.Message> is required`);
+  if (!type) throw new Error(`A Query Type is required`);
+  if (!query) throw new Error(`A Query is required`);
+
+  const valid = ["roles", "members"];
+  if (!valid.includes(type.toLowerCase()))
+    throw new Error(`The valid Query Types are [MEMBERS] and [ROLES]`);
+  const results = [];
+  switch (type) {
+    case "members":
+      const temp = msg.guild.members.cache.filter((m) =>
+        m.user.username.toLowerCase().includes(query.toLowerCase())
+      );
+      const allMem = temp.toJSON();
+      allMem.forEach((mem) => {
+        results.push(mem);
+      });
+      break;
+
+    case "roles":
+      const temporary = msg.guild.roles.cache.filter((r) =>
+        r.name.toLowerCase().includes(query.toLowerCase())
       );
       const allRoles = temporary.toJSON();
       allRoles.forEach((role) => {
@@ -114,5 +157,6 @@ module.exports = {
   formatTime,
   errorize,
   arrayRandomize,
-  multiQuery,
+  multiQueryStartsWith,
+  multiQueryIncludes,
 };
